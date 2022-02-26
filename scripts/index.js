70f91750d349eff3355c3ad78d9c57f080045938
Nicolas Nuccelli <nuccelli@hotmail.com>
2022-02-26 03:04:54 -0300

    botones funcionando, falta js del formulario

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+let encrBtn1repo = document.getElementById("encBtn1repo");
+let encrBtn1demo = document.getElementById("encBtn1demo");
+let ahorcBtn1repo = document.getElementById("ahorcBtn1repo");
+let ahorcBtn1demo = document.getElementById("ahorcBtn1demo");
+let encrBtn2repo = document.getElementById("encBtn2repo");
+let encrBtn2demo = document.getElementById("encBtn2demo");
+let ahorcBtn2repo = document.getElementById("ahorcBtn2repo");
+let ahorcBtn2demo = document.getElementById("ahorcBtn2demo");
+
+encrBtn1repo.addEventListener("click", function () {
+  window.open(
+    "https://github.com/nnfromthewindow/challengeencriptador",
+    "_blank"
+  );
+});
+encrBtn1demo.addEventListener("click", function () {
+  window.open(
+    "https://nnfromthewindow.github.io/challengeencriptador/",
+    "_blank"
+  );
+});
+ahorcBtn1repo.addEventListener("click", function () {
+  window.open(
+    "https://github.com/nnfromthewindow/challengeonejuegoahorcado",
+    "_blank"
+  );
+});
+ahorcBtn1demo.addEventListener("click", function () {
+  window.open(
+    "https://nnfromthewindow.github.io/challengeonejuegoahorcado/",
+    "_blank"
+  );
+});
+encrBtn2repo.addEventListener("click", function () {
+  window.open(
+    "https://github.com/nnfromthewindow/challengeencriptador",
+    "_blank"
+  );
+});
+encrBtn2demo.addEventListener("click", function () {
+  window.open(
+    "https://nnfromthewindow.github.io/challengeencriptador/",
+    "_blank"
+  );
+});
+ahorcBtn2repo.addEventListener("click", function () {
+  window.open(
+    "https://github.com/nnfromthewindow/challengeonejuegoahorcado",
+    "_blank"
+  );
+});
+ahorcBtn2demo.addEventListener("click", function () {
+  window.open(
+    "https://nnfromthewindow.github.io/challengeonejuegoahorcado/",
+    "_blank"
+  );
+});
