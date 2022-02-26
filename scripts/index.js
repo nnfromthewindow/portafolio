618136e1f9a6f662215891ab24176f0f6a1264a6
Nico <nuccelli@hotmail.com>
2022-02-26 14:57:12 -0300

    correccion de botones experiencia para mobile

--- scripts/index.js
+++ scripts/index.js
@@ -7,51 +7,52 @@ let encrBtn2demo = document.getElementById("encBtn2demo");
 let ahorcBtn2repo = document.getElementById("ahorcBtn2repo");
 let ahorcBtn2demo = document.getElementById("ahorcBtn2demo");
 
-encrBtn1repo.addEventListener("click", function () {
+encrBtn1repo.onclick = () => {
   window.open(
     "https://github.com/nnfromthewindow/challengeencriptador",
     "_blank"
   );
-});
-encrBtn1demo.addEventListener("click", function () {
+};
+
+encrBtn1demo.onclick = () => {
   window.open(
     "https://nnfromthewindow.github.io/challengeencriptador/",
     "_blank"
   );
-});
-ahorcBtn1repo.addEventListener("click", function () {
+};
+ahorcBtn1repo.onclick = () => {
   window.open(
     "https://github.com/nnfromthewindow/challengeonejuegoahorcado",
     "_blank"
   );
-});
-ahorcBtn1demo.addEventListener("click", function () {
+};
+ahorcBtn1demo.onclick = () => {
   window.open(
     "https://nnfromthewindow.github.io/challengeonejuegoahorcado/",
     "_blank"
   );
-});
-encrBtn2repo.addEventListener("click", function () {
+};
+encrBtn2repo.onclick = () => {
   window.open(
     "https://github.com/nnfromthewindow/challengeencriptador",
     "_blank"
   );
-});
-encrBtn2demo.addEventListener("click", function () {
+};
+encrBtn2demo.onclick = () => {
   window.open(
     "https://nnfromthewindow.github.io/challengeencriptador/",
     "_blank"
   );
-});
-ahorcBtn2repo.addEventListener("click", function () {
+};
+ahorcBtn2repo.onclick = () => {
   window.open(
     "https://github.com/nnfromthewindow/challengeonejuegoahorcado",
     "_blank"
   );
-});
-ahorcBtn2demo.addEventListener("click", function () {
+};
+ahorcBtn2demo.onclick = () => {
   window.open(
     "https://nnfromthewindow.github.io/challengeonejuegoahorcado/",
     "_blank"
   );
-});
+};
